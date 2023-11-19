7d44f1c06b2b3de1e60e1c899ee3b7247648b345
Person <localboxcrox@gmail.com>
2023-11-19 12:50:07 -0500

    stuff

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 rm -rf ./pkg/
 wasm-pack build --release --target web
-wasm-opt -O3 -o ./pkg/conway_life_bg.wasm ./pkg/conway_life_bg.wasm
+# wasm-opt -O3 -o ./pkg/conway_life_bg.wasm ./pkg/conway_life_bg.wasm
