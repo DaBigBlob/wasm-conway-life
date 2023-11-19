060d6b7940c42c59dabb4e05a4c8107956d67348
Person <localboxcrox@gmail.com>
2023-11-18 19:48:27 -0500

    stuff

--- www.sh
+++ www.sh
@@ -3,4 +3,5 @@
 rm -rf ./pkg/ && wasm-pack build --release --target web
 cp ./pkg/conway_life_bg.wasm ./www/dist/
 cp ./pkg/conway_life.js ./www/src
+cp ./www/src/index.html ./www/dist/index.html
 bun build ./www/src/index.js --target browser --minify --outfile ./www/dist/main.js
